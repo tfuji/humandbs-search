82775f020013b93bf026047662696ca07bd2475d
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2023-07-14 00:05:49 +0900

    Tentatively implemented to retrieve molecular data.

--- retrive_all.rb
+++ retrive_all.rb
@@ -2,6 +2,8 @@
 
 require 'csv'
 require 'json'
+require 'open-uri'
+require 'nokogiri'
 
 def parse (list_file)        
   @csv_rows = Hash.new { |h,k| h[k] = {} }
@@ -12,10 +14,47 @@ def parse (list_file)
     h['identifier'] = humID
     h['type'] = 'humandbs-research'
     h['dbXrefs'] = ids
-    @csv_rows[humID] = h 
+    h["molecular_data"] = retrive_moldata humID.gsub('.','-'), lang="ja"
+    @csv_rows[humID] = h
   end
   @csv_rows
 end
+
+###
+def retrive_moldata id, lang
+  #url = 'https://humandbs.biosciencedbc.jp/hum0147-v3'
+  url = "https://humandbs.biosciencedbc.jp/#{id}"
+  url = "https://humandbs.biosciencedbc.jp/en/#{id}" if lang =="en"
+  warn url
+  html = URI.open(url).read
+  doc = Nokogiri::HTML.parse(html)
+  items = Hash.new(0)
+  table = doc.css('table') 
+
+  moldata_table =[]
+  table.each_with_index do |tb, ib|
+    table_data = [] # この段階ではまだ1次元配列
+    moldata_flag = false
+
+    tb.css('tr:has(td)').each_with_index do |tr, ir|
+      table_data[ir] = [] # 配列の要素単位に配列を作成する(二次元化)
+
+      tr.css('td').each_with_index do |td, id|
+        table_data[ir][id] = td.text.strip
+        items[td.text.strip] += 1 if id == 0
+        moldata_flag = true if td.text.strip == "対象"
+      end
+    end
+    if moldata_flag
+      #puts "## テーブル#{ib+1}個目" if moldata_flag
+      #puts JSON.pretty_generate(table_data) if moldata_flag
+      moldata_table.push(table_data)
+    end
+  end
+  moldata_table
+end
+###
+
 
 hash = parse(ARGV.shift)
 puts  JSON.pretty_generate(hash)
